e0050057807c8d2accbac86439601cd73f82ca44
foxliu27 <54541374+foxliu27@users.noreply.github.com>
2024-10-19 22:54:31 +0800

    increase run words

--- fuel-cost/main.py
+++ fuel-cost/main.py
@@ -24,3 +24,7 @@ async def get_fuel_cost(
     fuel_cost = service.get_vessel_fuel_cost(year, carbon_unit_price, capacity_unit_price, fuel_list, fee_bate_cost_gap,
                                              lca_levy, lca_reward_cost_gap)
     return {"code": 200, "data": fuel_cost, "message": "查询成功"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(api, host="0.0.0.0", port=9089)
